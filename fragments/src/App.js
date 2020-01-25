ccf010b4e91470419cd86afab8f88ca095d80f6c
Rakesh <tech.konnection@gmail.com>
2020-01-25 12:28:13 -0500

    Fragment Commit

---
+++ fragments/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from 'react';
+import "./App.css";
+
+const Temp = () => {
+  return (
+    <>
+      <div>Hi</div>
+      <div>D</div></>
+  );
+}
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Temp />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
